02429133fa5f7d27650e684414cadca8c28a828d
XDCoder3289 <xdcoder3289@gmail.com>
2021-01-22 01:36:01 +0500

    Changing the home page

--- ayyaye/settings.py
+++ ayyaye/settings.py
@@ -29,15 +29,15 @@ with open('secret.key') as f:
 SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-CSRF_COOKIE_SECURE = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_BROWSER_XSS_FILTER = True
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-X_FRAME_OPTIONS = 'DENY'
+DEBUG = True
+# CSRF_COOKIE_SECURE = True
+# SECURE_CONTENT_TYPE_NOSNIFF = True
+# SECURE_BROWSER_XSS_FILTER = True
+# SECURE_SSL_REDIRECT = True
+# SESSION_COOKIE_SECURE = True
+# X_FRAME_OPTIONS = 'DENY'
 
-ALLOWED_HOSTS = ['www.mathsbehindml.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
