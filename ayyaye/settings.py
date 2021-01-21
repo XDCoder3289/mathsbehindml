e97412d5dbe4e8c92a3b3ed439855d931deee7c3
XDCoder3289 <xdcoder3289@gmail.com>
2021-01-21 21:55:15 +0500

    Changes

--- ayyaye/settings.py
+++ ayyaye/settings.py
@@ -23,10 +23,19 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3_t7or3n$w+upxsj^*-#72pt4x9g@lbyf)w6x#w52o#gyjk-wb'
+key = None
+with open('secret.key') as f:
+    key = f.read().strip()
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+CSRF_COOKIE_SECURE = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+X_FRAME_OPTIONS = 'DENY'
 
 ALLOWED_HOSTS = []
 
