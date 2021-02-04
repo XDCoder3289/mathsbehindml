87921811b6d259c4d5ee637e7756f4cefd33dfb2
XDCoder3289 <xdcoder3289@gmail.com>
2021-02-05 00:42:49 +0500

    adding search functionality

--- ai/admin.py
+++ ai/admin.py
@@ -3,5 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from ai.models import Category, Post
 
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ('title',)
+
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
